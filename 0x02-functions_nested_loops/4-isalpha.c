a4249ea0df54fa7c46d869a0371d0cd4f0382716
fahimnzeyimana <fahimnzeimana@gmail.com>
2022-12-13 07:43:39 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,9 +8,9 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
-	}
-	return (0);
+	else
+		return (0);
+
 }
