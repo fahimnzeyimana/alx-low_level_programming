6299bfb4bbca04d02481f9c2cde67ac3583290e6
fahimnzeyimana <fahimnzeimana@gmail.com>
2023-01-18 06:28:13 -0800

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,6 +9,7 @@
 int _strlen(char *s)
 {
 	int i;
+
 	i = 0;
 	while (s[i] != '\0')
 	{
@@ -28,6 +29,7 @@ int _strlen(char *s)
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
+
 	len = 0;
 	while (src[len] != '\0')
 	{
@@ -52,6 +54,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 	int len1, len2;
+
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
 	dog = malloc(sizeof(dog_t));
