777f3029f2bf070c561fcdd21a724f8eeac2663a
fahimnzeyimana <fahimnzeimana@gmail.com>
2023-01-31 06:14:36 -0800

    _

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
 
 /*
  * struct list_s - singly linked list
@@ -24,5 +23,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* LISTS_H */
+#endif
 
